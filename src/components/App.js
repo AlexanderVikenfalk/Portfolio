79be89de15590d1f85e8f8200a3e6ef843a98379
AlexanderVikenfalk <alexander.wikenfalk@gmail.com>
2017-10-04 11:53:09 +0200

    added clearfix

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Headline from './Headline.js'
 import Main from './Main.js'
 import '../styles/App.css';
+import Clearfix from 'react-clearfix';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Clearfix />
         <Headline />
         <Main />
      </div>
